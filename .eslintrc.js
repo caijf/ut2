7f84a1eae5afc87d2d69caf06fdd5bb09108bdc4
caijf <369756941@qq.com>
2023-05-11 13:44:30 +0800

    refactor: 添加规范配置

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-this-alias': 0,
+    '@typescript-eslint/no-explicit-any': 0
+  }
+};
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { version } = require('./package.json');
 const { COVERAGE_LOCAL } = process.env;
 
-const coverageConfig = COVERAGE_LOCAL === '1' ? {} : {
-  coverageReporters: ['text', 'cobertura'],
-}
+const coverageConfig =
+  COVERAGE_LOCAL === '1'
+    ? {}
+    : {
+        coverageReporters: ['text', 'cobertura']
+      };
 
 module.exports = {
   preset: 'ts-jest',
