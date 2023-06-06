062e74dbd344d301c449e7497da5f7b83223129b
caijf <369756941@qq.com>
2023-06-06 17:23:28 +0800

    docs: update

--- src/merge.ts
+++ src/merge.ts
@@ -52,6 +52,31 @@ function baseMerge<TObject, TSource>(
   return obj;
 }
 
+/**
+ * 递归合并 `source` 来源对象自身和继承的可枚举属性到 `object` 目标对象。
+ *
+ * 如果目标值存在，被解析为 `undefined` 的 `source` 来源对象属性将被跳过。数组和普通对象会递归合并，其他对象和值会被直接分配覆盖。
+ *
+ * @static
+ * @alias module:Object.merge
+ * @since 1.0.0
+ * @param {Object|Array} object 目标对象。
+ * @param {Object|Array} source 来源对象。
+ * @param {Function} [customizer] 自定义赋值函数。
+ * @returns {Object} 目标对象。
+ * @example
+ *
+ * const object = {
+ *   a: [{b: 2}, {d: 4}]
+ * }
+ *
+ * const other = {
+ *   a: [{c: 3},{e: 5}]
+ * }
+ *
+ * merge(object, other); // { a: [{b: 2, c: 3}, {d: 4, e: 5}] }
+ *
+ */
 function merge<TObject, TSource>(
   object: TObject,
   source: TSource,
