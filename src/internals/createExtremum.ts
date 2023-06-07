d5d7818164ac0294a42d78ff75bcd0afdc2e2cd8
caijf <369756941@qq.com>
2023-06-07 15:02:22 +0800

    refactor: 循环改用forEach

--- src/internals/createExtremum.ts
+++ src/internals/createExtremum.ts
@@ -10,17 +10,13 @@ function createExtremum<T, F extends (item: T) => any, K extends keyof T>(
     return;
   }
 
-  const length = array.length;
-  let index = -1;
   let result: T | undefined, computed: number | undefined;
 
   const internalIteratee =
     typeof iteratee === 'function' ? iteratee : (value: any) => value[iteratee];
 
-  while (++index < length) {
-    const value = array[index];
+  array.forEach((value) => {
     const current = internalIteratee(value);
-
     if (
       current != null &&
       (computed === undefined
@@ -30,7 +26,7 @@ function createExtremum<T, F extends (item: T) => any, K extends keyof T>(
       computed = current;
       result = value;
     }
-  }
+  });
 
   return result;
 }
