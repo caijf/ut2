0d2853e3409f67521830afc0d3f0ea1be255906b
caijf <369756941@qq.com>
2023-06-11 12:16:54 +0800

    feat: 新增shuffle方法

---
+++ src/shuffle.ts
@@ -0,0 +1,39 @@
+import isArray from './isArray';
+import randomInt from './randomInt';
+
+/**
+ * 创建一个被打乱的数组。使用 [Fisher-Yates shuffle](https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle) 版本。
+ *
+ * @static
+ * @alias module:Array.shuffle
+ * @since 1.0.0
+ * @param {Array} array 要打乱的数组。
+ * @returns {Array} 打乱的数组。
+ * @example
+ *
+ * shuffle([1, 2, 3, 4]); // [2, 4, 3, 1]
+ *
+ * shuffle([1, 2, 3, 4]); // [3, 2, 1, 4]
+ *
+ */
+function shuffle<T>(array: T[]) {
+  if (!isArray(array) || array.length < 1) {
+    return [];
+  }
+
+  const result = array.slice();
+  const length = result.length;
+  const lastIndex = length - 1;
+  let index = -1;
+
+  while (++index < length) {
+    const rand = randomInt(index, lastIndex);
+    const value = result[rand];
+    result[rand] = result[index];
+    result[index] = value;
+  }
+
+  return result;
+}
+
+export default shuffle;
---
+++ test/shuffle.test.ts
@@ -0,0 +1,29 @@
+import { shuffle, times } from '../src';
+import { symbol } from './_utils';
+
+describe('shuffle', () => {
+  const array = [1, 2, 3];
+
+  it('返回一个新数组', () => {
+    expect(shuffle(array)).not.toBe(array);
+  });
+
+  it('返回一个包含所有值的数组', () => {
+    expect(shuffle(array).sort()).toEqual(array);
+  });
+
+  it('打乱最小的数组', () => {
+    const smallArray = [1, 2];
+    times(1000, () => shuffle(smallArray)).forEach((item) => {
+      expect(item).toEqual(item[0] === 1 ? [1, 2] : [2, 1]);
+    });
+  });
+
+  it('错误的参数', () => {
+    const values = [1, '', 'a', { a: 1 }, null, undefined, NaN, symbol];
+    values.forEach((item) => {
+      // @ts-ignore
+      expect(shuffle(item)).toEqual([]);
+    });
+  });
+});
