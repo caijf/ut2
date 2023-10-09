e3947c41129054daeb3ba7086a05c1ebafaf160e
caijf <369756941@qq.com>
2023-10-09 14:03:54 +0800

    fix: 全局对象添加 globalThis

--- src/internals/root.ts
+++ src/internals/root.ts
@@ -1,6 +1,7 @@
+const freeGlobalThis = typeof globalThis === 'object' && globalThis && globalThis.Object === Object && globalThis;
 const freeGlobal = typeof global === 'object' && global && global.Object === Object && global;
 const freeSelf = typeof self === 'object' && self && self.Object === Object && self;
 
-const root = freeGlobal || freeSelf || Function('return this')() || {};
+const root = freeGlobalThis || freeGlobal || freeSelf || Function('return this')() || {};
 
 export default root;
