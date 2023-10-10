2caff3b9036c858e0a5cee212ac5f895fbc73457
caijf <369756941@qq.com>
2023-10-10 19:53:49 +0800

    test: 补充测试用例

--- test/_inspect/internals/root-self.test.ts
+++ test/_inspect/internals/root-self.test.ts
@@ -1,11 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
 jest.mock('../../../src/internals/native.ts', () => {
   const originalModule = jest.requireActual('../../../src/internals/native.ts');
 
   return {
     ...originalModule,
     globalThisExisted: false,
-    globalExisted: false,
-    selfExisted: false
+    globalExisted: false
   };
 });
 
