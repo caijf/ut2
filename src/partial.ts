bc9e17798213ecccaaff44c37738e894c66f123c
caijf <369756941@qq.com>
2024-04-16 17:06:09 +0800

    refactor(partial): 支持参数占位符

--- src/partial.ts
+++ src/partial.ts
@@ -1,105 +1,57 @@
-function partial<TS extends any[], R>(func: (...ts: TS) => R): (...ts: TS) => R;
-function partial<TS extends any[], T1, R>(
-  func: (t1: T1, ...ts: TS) => R,
-  arg1: T1
-): (...ts: TS) => R;
-function partial<TS extends any[], T1, T2, R>(
-  func: (t1: T1, t2: T2, ...ts: TS) => R,
-  t1: T1,
-  t2: T2
-): (...ts: TS) => R;
-function partial<TS extends any[], T1, T2, T3, R>(
-  func: (t1: T1, t2: T2, t3: T3, ...ts: TS) => R,
-  t1: T1,
-  t2: T2,
-  t3: T3
-): (...ts: TS) => R;
-function partial<TS extends any[], T1, T2, T3, T4, R>(
-  func: (t1: T1, t2: T2, t3: T3, t4: T4, ...ts: TS) => R,
-  t1: T1,
-  t2: T2,
-  t3: T3,
-  t4: T4
-): (...ts: TS) => R;
-function partial<TS extends any[], T1, T2, T3, T4, T5, R>(
-  func: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, ...ts: TS) => R,
-  t1: T1,
-  t2: T2,
-  t3: T3,
-  t4: T4,
-  t5: T5
-): (...ts: TS) => R;
-function partial<TS extends any[], T1, T2, T3, T4, T5, T6, R>(
-  func: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, ...ts: TS) => R,
-  t1: T1,
-  t2: T2,
-  t3: T3,
-  t4: T4,
-  t5: T5,
-  t6: T6
-): (...ts: TS) => R;
-function partial<TS extends any[], T1, T2, T3, T4, T5, T6, T7, R>(
-  func: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, ...ts: TS) => R,
-  t1: T1,
-  t2: T2,
-  t3: T3,
-  t4: T4,
-  t5: T5,
-  t6: T6,
-  t7: T7
-): (...ts: TS) => R;
-function partial<TS extends any[], T1, T2, T3, T4, T5, T6, T7, T8, R>(
-  func: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8, ...ts: TS) => R,
-  t1: T1,
-  t2: T2,
-  t3: T3,
-  t4: T4,
-  t5: T5,
-  t6: T6,
-  t7: T7,
-  t8: T8
-): (...ts: TS) => R;
-function partial<TS extends any[], T1, T2, T3, T4, T5, T6, T7, T8, T9, R>(
-  func: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8, t9: T9, ...ts: TS) => R,
-  t1: T1,
-  t2: T2,
-  t3: T3,
-  t4: T4,
-  t5: T5,
-  t6: T6,
-  t7: T7,
-  t8: T8,
-  t9: T9
-): (...ts: TS) => R;
-function partial<TS extends any[], T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R>(
-  func: (
-    t1: T1,
-    t2: T2,
-    t3: T3,
-    t4: T4,
-    t5: T5,
-    t6: T6,
-    t7: T7,
-    t8: T8,
-    t9: T9,
-    t10: T10,
-    ...ts: TS
-  ) => R,
-  t1: T1,
-  t2: T2,
-  t3: T3,
-  t4: T4,
-  t5: T5,
-  t6: T6,
-  t7: T7,
-  t8: T8,
-  t9: T9,
-  t10: T10
-): (...ts: TS) => R;
+/* eslint-disable prefer-rest-params */
+import { arrayProtoSlice } from './internals/native';
+
+interface PartialPlaceholder {
+  __ut2_partial__: number;
+}
+
+const PLACEHOLDER: PartialPlaceholder = {
+  __ut2_partial__: 1
+};
+
+type Placeholder = PartialPlaceholder;
+type Function0<R> = () => R;
+type Function1<T1, R> = (t1: T1) => R;
+type Function2<T1, T2, R> = (t1: T1, t2: T2) => R;
+type Function3<T1, T2, T3, R> = (t1: T1, t2: T2, t3: T3) => R;
+type Function4<T1, T2, T3, T4, R> = (t1: T1, t2: T2, t3: T3, t4: T4) => R;
+
+interface Partial {
+  <T1, T2, R>(func: Function2<T1, T2, R>, plc1: Placeholder, arg2: T2): Function1<T1, R>;
+  <T1, T2, R>(func: Function2<T1, T2, R>, arg1: T1, arg2: T2): Function0<R>;
+  <T1, T2, T3, R>(func: Function3<T1, T2, T3, R>, plc1: Placeholder, arg2: T2): Function2<T1, T3, R>;
+  <T1, T2, T3, R>(func: Function3<T1, T2, T3, R>, plc1: Placeholder, plc2: Placeholder, arg3: T3): Function2<T1, T2, R>;
+  <T1, T2, T3, R>(func: Function3<T1, T2, T3, R>, arg1: T1, plc2: Placeholder, arg3: T3): Function1<T2, R>;
+  <T1, T2, T3, R>(func: Function3<T1, T2, T3, R>, plc1: Placeholder, arg2: T2, arg3: T3): Function1<T1, R>;
+  <T1, T2, T3, R>(func: Function3<T1, T2, T3, R>, arg1: T1, arg2: T2, arg3: T3): Function0<R>;
+  <T1, T2, T3, T4, R>(func: Function4<T1, T2, T3, T4, R>, plc1: Placeholder, arg2: T2): Function3<T1, T3, T4, R>;
+  <T1, T2, T3, T4, R>(func: Function4<T1, T2, T3, T4, R>, plc1: Placeholder, plc2: Placeholder, arg3: T3): Function3<T1, T2, T4, R>;
+  <T1, T2, T3, T4, R>(func: Function4<T1, T2, T3, T4, R>, arg1: T1, plc2: Placeholder, arg3: T3): Function2<T2, T4, R>;
+  <T1, T2, T3, T4, R>(func: Function4<T1, T2, T3, T4, R>, plc1: Placeholder, arg2: T2, arg3: T3): Function2<T1, T4, R>;
+  <T1, T2, T3, T4, R>(func: Function4<T1, T2, T3, T4, R>, arg1: T1, arg2: T2, arg3: T3): Function1<T4, R>;
+  <T1, T2, T3, T4, R>(func: Function4<T1, T2, T3, T4, R>, plc1: Placeholder, plc2: Placeholder, plc3: Placeholder, arg4: T4): Function3<T1, T2, T3, R>;
+  <T1, T2, T3, T4, R>(func: Function4<T1, T2, T3, T4, R>, arg1: T1, plc2: Placeholder, plc3: Placeholder, arg4: T4): Function2<T2, T3, R>;
+  <T1, T2, T3, T4, R>(func: Function4<T1, T2, T3, T4, R>, plc1: Placeholder, arg2: T2, plc3: Placeholder, arg4: T4): Function2<T1, T3, R>;
+  <T1, T2, T3, T4, R>(func: Function4<T1, T2, T3, T4, R>, arg1: T1, arg2: T2, plc3: Placeholder, arg4: T4): Function1<T3, R>;
+  <T1, T2, T3, T4, R>(func: Function4<T1, T2, T3, T4, R>, plc1: Placeholder, plc2: Placeholder, arg3: T3, arg4: T4): Function2<T1, T2, R>;
+  <T1, T2, T3, T4, R>(func: Function4<T1, T2, T3, T4, R>, arg1: T1, plc2: Placeholder, arg3: T3, arg4: T4): Function1<T2, R>;
+  <T1, T2, T3, T4, R>(func: Function4<T1, T2, T3, T4, R>, plc1: Placeholder, arg2: T2, arg3: T3, arg4: T4): Function1<T1, R>;
+  <T1, T2, T3, T4, R>(func: Function4<T1, T2, T3, T4, R>, arg1: T1, arg2: T2, arg3: T3, arg4: T4): Function0<R>;
+  <TS extends any[], R>(func: (...ts: TS) => R): (...ts: TS) => R;
+  <TS extends any[], T1, R>(func: (t1: T1, ...ts: TS) => R, arg1: T1): (...ts: TS) => R;
+  <TS extends any[], T1, T2, R>(func: (t1: T1, t2: T2, ...ts: TS) => R, t1: T1, t2: T2): (...ts: TS) => R;
+  <TS extends any[], T1, T2, T3, R>(func: (t1: T1, t2: T2, t3: T3, ...ts: TS) => R, t1: T1, t2: T2, t3: T3): (...ts: TS) => R;
+  <TS extends any[], T1, T2, T3, T4, R>(func: (t1: T1, t2: T2, t3: T3, t4: T4, ...ts: TS) => R, t1: T1, t2: T2, t3: T3, t4: T4): (...ts: TS) => R;
+  placeholder: Placeholder;
+  _: Placeholder;
+}
+
 /**
  * 创建一个函数。该函数调用 `func` ，并传入预设的 `args` 参数。
  *
- * @static
+ *  `partial._` 或 `partial.placeholder` 可用作部分参数的占位符。
+ *
+ * @function
  * @alias module:Function.partial
  * @since 1.0.0
  * @param {Function} func 需要预设的函数。
@@ -112,15 +64,28 @@ function partial<TS extends any[], T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R>(
  * }
  *
  * const sayHelloTo = partial(greet, 'hello');
- *
  * sayHelloTo('jeff'); // 'hello jeff'
  *
+ * const greetJeff = partial(greet, partial._, 'jeff');
+ * greetJeff('hi'); // 'hi jeff'
+ *
  */
-function partial(func: any, ...args: any[]) {
-  return function (...remainingArgs: any[]) {
+const partial: Partial = function (func: any) {
+  const argsOrig = arrayProtoSlice.call(arguments, 1);
+
+  return function () {
+    const args = [];
+    const argsPartial = arrayProtoSlice.call(arguments);
+
+    for (let i = 0; i < argsOrig.length; i++) {
+      args[i] = argsOrig[i] === PLACEHOLDER ? argsPartial.shift() : argsOrig[i];
+    }
+
     // @ts-ignore
-    return func.apply(this, args.concat(remainingArgs));
+    return func.apply(this, args.concat(argsPartial));
   };
-}
+};
+
+partial.placeholder = partial._ = PLACEHOLDER;
 
 export default partial;
--- test/partial.test.ts
+++ test/partial.test.ts
@@ -20,9 +20,20 @@ describe('partial', () => {
 
     const fn4 = partial(abc, ...args);
     expect(fn4()).toEqual(abc(...args));
+
+    const fn5 = partial(identity<string>, '1');
+    expect(fn5()).toBe('1');
   });
 
-  it('测试 `arguments` 数量 ', () => {
+  it('创建一个函数，并添加参数', () => {
+    const fn = function (a: string, b: string) {
+      return [a, b];
+    };
+    const par = partial(fn, 'a');
+    expect(par('b')).toEqual(['a', 'b']);
+  });
+
+  it('`arguments` 数量 ', () => {
     const fn = partial(function () {
       return arguments.length;
     });
@@ -33,7 +44,7 @@ describe('partial', () => {
     expect(fn(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)).toBe(12);
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const fn1 = partial(function (a, b, c) {});
+    const fn1 = partial(function (a: any, b: any, c: any) {});
     expect(fn1.length).toBe(0);
   });
 
@@ -49,5 +60,23 @@ describe('partial', () => {
     expect(per1('a')).toBe('hi a');
     expect(per2()).toBe('hi cai');
     expect(per3()).toBe('hi jeff');
+
+    const per11 = partial(greet, partial._, 'jeff');
+    const per22 = partial(per11, 'hi');
+    const per33 = partial(per11, 'hello');
+    expect(per11('a')).toBe('a jeff');
+    expect(per22()).toBe('hi jeff');
+    expect(per33()).toBe('hello jeff');
+  });
+
+  it('参数占位符', () => {
+    function abc(a: string, b: number, c: boolean) {
+      return [a, b, c];
+    }
+    const abcPartial1 = partial(abc, partial._, partial.placeholder, false);
+    const abcPartial2 = partial(abcPartial1, partial._, 1);
+    const abcPartial3 = partial(abcPartial2, 'a');
+
+    expect(abcPartial3()).toEqual(['a', 1, false]);
   });
 });
