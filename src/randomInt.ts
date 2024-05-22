673618e2e1d9f92c1a3db38f4d7fda9f7601683c
caijf <369756941@qq.com>
2024-05-22 14:18:45 +0800

    docs: update

--- src/randomInt.ts
+++ src/randomInt.ts
@@ -9,8 +9,8 @@ import toFinite from './toFinite';
  * @static
  * @alias module:Number.randomInt
  * @since 1.0.0
- * @param {number} [lower=0] 下限。
- * @param {number} [upper=1] 上限。
+ * @param {number} [lower=0] 下限，默认 0 。
+ * @param {number} [upper=1] 上限，默认 1 。
  * @returns {number} 随机整数。
  * @example
  *
